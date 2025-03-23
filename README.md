a5cf9dc0bd7ab5d5bc11f2dfd1fcdc46a9485ba2
srikanth <Srikanth.Hurali@thomsonreuters.com>
2025-03-23 12:36:43 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 js-action-pr-giphy-comment
+
--- index.js
+++ index.js
@@ -2,3 +2,32 @@ const { Octokit } = require('@octokit/rest');
 const Giphy = require('giphy-api');
 const core = require('@actions/core');
 const github = require('@actions/github');
+
+async function run() {
+    try {
+      const githubToken = core.getInput('github-token');
+      const giphyApiKey = core.getInput('giphy-api-key');
+  
+      const octokit = new Octokit({ auth: githubToken });
+      const giphy = new GiphyFetch(giphyApiKey);
+  
+      const context = github.context;
+      const { owner, repo, number } = context.issue;
+      const prComment = await giphy.random({ tag: 'thank you', rating: 'g' });
+  
+      await octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: number,
+        body: `### PR - ${number} \n ### Thank you for the contribution! \n ![Giphy](${prComment.data.images.downsized.url})`,
+      });
+  
+      core.setOutput('comment-url', `${prComment.data.images.downsized.url}`);
+      console.log(`Giphy GIF comment added successfully! Comment URL: ${prComment.data.images.downsized.url}`);
+    } catch (error) {
+      console.error('Error:', error);
+      Process.exit(1);
+    }
+  }
+  
+  run();
